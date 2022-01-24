076c2891d5a6bfc6b7cd3475d36c973cd9328c50
Jack <jacktiebie45@gmail.com>
2022-01-24 22:41:57 +0100

    Adding Code

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-var button = document.querySelector("#button_start");
 
 
-
-
-if (button.click) {
-    var answer = prompt("Who is this pokemon?");
-}
+function startClick(){  
+    var pokemon = prompt("Name of this pokemon is?");
+    var pikachu = "pikachu";
+    var lol = document.getElementById("title");
+    var pictureCheck = document.getElementById("pokemonPic");
+      
+      
+      if (pokemon == pikachu) {
+    pictureCheck.src = "https://images.unsplash.com/photo-1564038057544-f9ca83c46804?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzA1OTg2NQ&ixlib=rb-1.2.1&q=85";
+     
+     lol.innerHTML = "Good!";
+        
+      } else {
+       lol.innerHTML = "Wrong";
+       pictureCheck.src = "https://images.unsplash.com/photo-1527631120902-378417754324?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MzA1OTg1MA&ixlib=rb-1.2.1&q=85";
+        
+      }
+        }  
+    
+    function startReset() {
+      document.location.reload(true)
+    }
